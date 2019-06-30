d2f3b29f675e3456d6217ec16a4ec1228b93f33b
kbpsystem <koleen.bp@outlook.com>
2019-06-30 08:04:11 +0800

    adding author data

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from catalog.models import Author, Book, BookInstance, Genre
 
 # Register your models here.
+app_models = [Author, Book, BookInstance, Genre]
+
+admin.site.register(app_models) 
